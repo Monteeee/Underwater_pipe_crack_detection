8cc669b6d733209285942f8926c9d4d3cc5afe8f
Monteeee <montemiracle@live.com>
2019-03-06 23:41:38 +0100

    skeleton of better_proposal

---
+++ utils/better_proposal.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+class BetterProposal:
+    @staticmethod
+    def better_sliding_window(image_shape, window_size):
+        """this function serves demo by providing better sliding window.
+        It should return a group of windows, i.e. a group of 4x2 ndarray showing pixel coordinate
+        of window corners"""
+        windows = list()
+
+        return windows
+
+    @staticmethod
+    def better_proposal(pos_score_group):
+        """this function serves demo by providing better proposals.
+        It should receive a group of position-score pair, representing raw proposals for detection.
+        Then this function will find better proposals by summarizing raw proposals"""
+
+        better_prop = list()
+        return better_prop
+

2ff9a4568d5e124f06dc4d6b94888de1f98f3caf
Yipai Du <yipai.du@outlook.com>
2019-03-17 17:36:49 +0100

    add metrics

--- networks/shallow-network/keras_cnn.py
+++ networks/shallow-network/keras_cnn.py
@@ -9,62 +9,60 @@ np.random.seed(1337)  # for reproducibility
 
 img_width, img_height = 80, 80
 
-train_data_dir = 'image/train'
-validation_data_dir = 'image/valid'
+train_data_dir = "C:/Users/yipai.du/Downloads/large data-20190313T201506Z-001 - Keras_format/sorted/train"
+validation_data_dir = "C:/Users/yipai.du/Downloads/large data-20190313T201506Z-001 - Keras_format/sorted/valid"
 
 steps_per_epoch = 30
 epochs = 100
 batch_size = 48
 
-if K.image_data_format() == 'channels_first':
+if K.image_data_format() == "channels_first":
     input_shape = (3, img_width, img_height)
 else:
     input_shape = (img_width, img_height, 3)
 
 model = Sequential()
 model.add(Conv2D(64, (3, 3), input_shape=input_shape))
-model.add(Activation('relu'))
+model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
+model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
+model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
 model.add(Dense(256))
-model.add(Activation('relu'))
+model.add(Activation("relu"))
 # model.add(Dropout(0.5))
 model.add(Dense(4))
-model.add(Activation('sigmoid'))
+model.add(Activation("sigmoid"))
 
-model.compile(
-    loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
 train_datagen = ImageDataGenerator(
-    rescale=1. / 255,
-    shear_range=0.2,
-    horizontal_flip=True,
-    vertical_flip=True)
+    rescale=1.0 / 255, shear_range=0.2, horizontal_flip=True, vertical_flip=True
+)
 
-valid_datagen = ImageDataGenerator(rescale=1. / 255)
+valid_datagen = ImageDataGenerator(rescale=1.0 / 255)
 
 train_generator = train_datagen.flow_from_directory(
-    train_data_dir,
-    target_size=(img_width, img_height),
-    batch_size=batch_size)
+    train_data_dir, target_size=(img_width, img_height), batch_size=batch_size
+)
 
 validation_generator = valid_datagen.flow_from_directory(
-    validation_data_dir,
-    target_size=(img_width, img_height),
-    batch_size=batch_size)
+    validation_data_dir, target_size=(img_width, img_height), batch_size=batch_size
+)
 
 model.fit_generator(
     train_generator,
     epochs=epochs,
-    validation_data=validation_generator)
+    steps_per_epoch=steps_per_epoch,
+    validation_steps=1,
+    validation_data=validation_generator,
+)
 
-model.save('my.h5')
+model.save("my.h5")
--- networks/shallow-network/predict.py
+++ networks/shallow-network/predict.py
@@ -1,32 +1,71 @@
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import load_model
-from sklearn.metrics import classification_report, confusion_matrix
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import log_loss, roc_auc_score, f1_score, roc_curve
+from sklearn.metrics import accuracy_score, recall_score, confusion_matrix
+from sklearn.metrics import auc
+from sklearn.preprocessing import label_binarize
+from sklearn.metrics import classification_report
+from itertools import cycle
 
 img_width, img_height = 80, 80
 
-test_data_dir = 'image/test'
+test_data_dir = "C:/Users/yipai.du/Downloads/large data-20190313T201506Z-001 - Keras_format/sorted/test"
 
 batch_size = 64
-test_datagen = ImageDataGenerator(rescale=1. / 255)
+test_datagen = ImageDataGenerator(rescale=1.0 / 255)
 
 test_generator = test_datagen.flow_from_directory(
     test_data_dir,
     target_size=(img_width, img_height),
     batch_size=batch_size,
-    shuffle=False)
+    shuffle=False,
+)
 
-model = load_model('my.h5')
 
-#Confution Matrix and Classification Report
-Y_pred = model.predict_generator(test_generator)
-y_pred = np.argmax(Y_pred, axis=1)
-print('Confusion Matrix')
-C = confusion_matrix(test_generator.classes, y_pred)
-C_n = C.astype('float') / C.sum(axis=1)[:, np.newaxis]
-print(C_n)
-# print('Classification Report')
-# target_names = ['0', '1', '2', '3']
-# print(
-#     classification_report(
-#         test_generator.classes, y_pred, target_names=target_names))
+model = load_model("my.h5")
+n_classes = 4
+# Confution Matrix and Classification Report
+y_score = model.predict_generator(test_generator, steps=int(test_generator.samples/batch_size+0.5))
+y_pred = np.argmax(y_score, axis=1)
+y_true = test_generator.classes
+print("test Acc", round(accuracy_score(y_true, y_pred), 4))
+y_one_hot = label_binarize(y_true, np.arange(n_classes))
+# print("test LogLoss", round(log_loss(y_true, y_pred), 4))
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(n_classes):
+    fpr[i], tpr[i], _ = roc_curve(y_one_hot[:, i], y_score[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
+
+# fpr, tpr, thresholds = roc_curve(y_one_hot.ravel(), y_score.ravel())
+lw = 2
+plt.figure()
+
+colors = cycle(["aqua", "darkorange", "cornflowerblue"])
+for i, color in zip(range(n_classes), colors):
+    plt.plot(
+        fpr[i],
+        tpr[i],
+        color=color,
+        lw=lw,
+        label="ROC curve of class {0} (area = {1:0.2f})".format(i, roc_auc[i]),
+    )
+
+plt.plot([0, 1], [0, 1], "k--", lw=lw)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.title("Some extension of Receiver operating characteristic to multi-class")
+plt.legend(loc="lower right")
+plt.show()
+
+print("test Confusion Matrix \n", confusion_matrix(y_true, y_pred))
+target_names = ["class 0", "class 1", "class 2"]
+print(
+    "test classification report \n"
+    + classification_report(y_true, y_pred, target_names=target_names)
+)
